169771ca1d96cb6b410d5612b23074957e7f0e46
praveenreddy2005 <bandipraveenreddy29@gmail.com>
2025-01-09 11:53:44 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,53 @@
+//function without parameters:
+//function demo(){
+//    console.log("function without parameters")
+//}
+//demo();
+
+
+//Function with parameters:
+
+function userDetails(name,age,city){
+    console.log(name);
+    console.log(age);
+    console.log(city);
+}
+userDetails("Praveen",20,"Hyderabad");
+
+
+//anonymous function:
+//function(){
+//    console.log("Anonymous function");
+//}
+
+//function experssion:
+//let x=function(){
+//   console.log("Function experssion is anonymous");
+//};
+//x();
+
+
+//ARROW FUNCTION
+//function demo(){
+//    console.log("Hello");
+//}
+//demo();
+
+// let x=()=>console.log("Arrow function");
+// x();
+
+// let x=_=()=>console.log("Arrow function");
+// x();
+
+// let x==()=>console.log("Arrow function");
+// x();
+
+// let x=(a,b)=>console.log(a+b)
+// x(5,5);
+
+let x=_=>{console.log("Hi");
+        console.log("Hello");
+        console.log("Bye");}
+    x();
+    {
+};
