c55b6bca769aaa5aab46f5026773b06c1d754b92
praveenreddy2005 <bandipraveenreddy29@gmail.com>
2025-01-24 22:19:52 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -5,7 +5,18 @@
 // element.innerText = "G+"
 // console.log(element);
 
-let ele = document.getElementById("test");
+// let ele = document.getElementsByClassName("test");
 // ele.innerText= "<h1>Header</h1>"
-ele.innerHTML = "<h1>Header</h1>"
+// ele.innerHTML = "<h1>Header</h1>"
+// console.log(ele);
+// console.log(Array.isArray(ele));
+// ele[0].innerText="Hello"
+// // ele[0].style.backgroundColor="tomato";
+
+
+// let x=[...ele];
+// x.map(element=>{
+//     element.style.backgroundColor="orange"
+// })
+let ele = document.querySelectorAll(".test");
 console.log(ele);
