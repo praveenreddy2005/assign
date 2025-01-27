9b3dea05d81c2995b0d49a3a0fbb0bcbe7e33071
praveenreddy2005 <bandipraveenreddy29@gmail.com>
2025-01-27 13:00:14 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -15,8 +15,82 @@
 
 
 // let x=[...ele];
-// x.map(element=>{
+// x.map(element=>{s
 //     element.style.backgroundColor="orange"
 // })
-let ele = document.querySelectorAll(".test");
-console.log(ele);
+// let ele = document.querySelectorAll(".test");
+// console.log(ele);
+
+// let ele =document.createElement("h1");
+// ele.innerText="Dynamic element of html element";
+// ele.id="demo";
+// console.log(ele);
+
+// let image=document.createElement("img");
+// image.src="C:\Users\Admin\OneDrive\Pictures\res1.jpg";
+// console.log(image);
+
+// document.body.appendChild(ele);
+// document.body.appendChild(image)
+
+// let form = document.querySelector("form");
+// let username = document.getElementById("uName");
+// let mail = document.getElementById("uMail");
+// let psw = document.getElementById("uPass");
+
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     // console.log(event);
+//     // console.log("form submitted");
+//     let uname = username.value;
+//     let uemail = mail.value;
+//     let upass = psw.value;
+//     let userDetails = {
+//         uname, uemail, upass
+//     }
+//     console.log(userDetails);
+//     localStorage.setItem("userData", JSON.stringify(userDetails));
+// });
+
+let mainEle = document.createElement("div");
+mainEle.setAttribute("id", "mainBlock");
+mainEle.style.border = "2px solid red";
+mainEle.style.width = "550px";
+mainEle.style.height = "550px";
+
+console.log(mainEle);
+
+let topEle = document.createElement("div");
+topEle.setAttribute("class", "topBlock");
+// console.log(topEle);
+
+let image = document.createElement("img");
+image.src = "https://cdn.pixabay.com/photo/2023/02/09/16/43/cat-7779237_640.jpg";
+image.style.width = "550px";
+
+let bottomEle = document.createElement("div");
+bottomEle.setAttribute("class", "bottomBlock");
+// console.log(bottomEle);
+
+let h1 = document.createElement("h1");
+h1.innerText = "CAT";
+h1.style.textAlign = "center";
+
+let btn = document.createElement("button");
+btn.innerText = "View More";
+btn.style.border = "none";
+btn.style.padding = "20px";
+btn.style.backgroundColor = "dodgerblue";
+btn.style.color = "white";
+
+bottomEle.appendChild(h1);
+bottomEle.appendChild(btn);
+
+topEle.appendChild(image);
+mainEle.appendChild(topEle);
+mainEle.appendChild(bottomEle);
+document.body.appendChild(mainEle);
+
+
+
+
